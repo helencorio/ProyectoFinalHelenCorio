84de9d6d14af5364119d717c2968466dcdc72eee
Helen Corio <helenncorio@gmail.com>
2023-04-01 20:34:52 -0300

    Bd

--- Proyecto/recetas/models.py
+++ Proyecto/recetas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Dulce(models.Model):
+    nombrereceta= models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.id} - {self.nombrereceta}"
+    
+
+
+
+
